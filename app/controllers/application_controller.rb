44b2cb63fcabc5316b3b807af0b10dbd09169344
21technology <sierrapollyanna@gmail.com>
2018-09-13 12:43:23 +0900

    fix error on signup confirmation -debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #before_action :authenticate_tenant!
 
      ##    milia defines a default max_tenants, invalid_tenant exception handling
      ##    but you can override these if you wish to handle directly
